bab36a165515d2df94c0725e515108eb4fde0cd9
Jonathan Pham <jonpham247@gmail.com>
2014-10-12 17:46:54 -0700

    Add TestDrive file for DotComBustGame

---
+++ hf_j2_battlecom_simple/src/hf_j2_battlecom/DotComBustGameTestDrive.java
@@ -0,0 +1,13 @@
+package hf_j2_battlecom;
+
+public class DotComBustGameTestDrive {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
